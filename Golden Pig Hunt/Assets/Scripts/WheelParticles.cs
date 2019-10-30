533730c6e9c6815ab0fd837507ec5837312a2222
bizonAI <d.robra@online.de>
2019-10-30 23:29:09 +0100

    pushi pushi push

--- Golden Pig Hunt/Assets/Scripts/WheelParticles.cs
+++ Golden Pig Hunt/Assets/Scripts/WheelParticles.cs
@@ -5,22 +5,13 @@ using UnityEngine;
 public class WheelParticles : MonoBehaviour {
 
     public Transform player;
-    public GameObject[] dustParticlePos;
     public GameObject dustParticleEffect;
-
-    private Vector2 playersPos;
 
 	void Update ()
     {
-        playersPos = player.position;
-
         if(player.hasChanged)
         {
-            for (int i = 0; i < dustParticlePos.Length; i++)
-            {
-                Instantiate(dustParticleEffect, dustParticlePos[i].transform.position, Quaternion.identity);
-            }
-            player.hasChanged = false;
+            Debug.Log("Pos has changed");
         }
 	}
 }
