cde7e1f4f6c96d96c35206ef72de2727dd4095c9
diego <diego@i3>
2019-11-19 00:57:34 +0100

    speed is calculated

--- Golden Pig Hunt/Assets/Scripts/Spawner.cs
+++ Golden Pig Hunt/Assets/Scripts/Spawner.cs
@@ -11,15 +11,16 @@ public class Spawner : MonoBehaviour
     public GameObject goldenPig;
 
     //setting
-    private float responseTime = 1f;
+    private float responseTime = 5f;
     private float minResponseTime = .01f;
-    private float maxHeight = 5.0f;
+    private float maxHeight = 4f;
     private float lastPigTime = 0f;
 
     //decaying function
     // f(x) = 1/dx + c
     private float timeOffset = 0f;
-    private float decayRate = .02f;
+    private float decayRate = .005f;
+
 
     private void Start()
     {
@@ -34,57 +35,14 @@ public class Spawner : MonoBehaviour
         if (!Player.died)
         {
             responseTime = 1 / (decayRate * (currentTime + timeOffset)) + minResponseTime;
-            if (lastPigTime < currentTime){
+            if (lastPigTime < currentTime)
+            {
                 lastPigTime = currentTime + responseTime;
                 Vector2 spawnPos = new Vector2(transform.position.x, Random.Range(-maxHeight, maxHeight));
-                Instantiate(goldenPig, spawnPos, Quaternion.identity);
+                GameObject go = Instantiate(goldenPig, spawnPos, Quaternion.identity);
+                go.SendMessage("SetSpeed", 10 / responseTime);
+                //Debug.Log(responseTime + " : " + 10 / responseTime);
             }
-            // if (timeBtwSpawnGolden <= 0 + rndSpawnTimeGolden)
-            // {
-            //     //int rnd = Random.Range(0, spawnObjs.Length);
-
-                //     spawnPos = new Vector2(transform.position.x, Random.Range(-maxHeight, maxHeight));
-
-                //     Instantiate(goldenPig, spawnPos, Quaternion.identity);
-                //     rndSpawnTimeGolden = Random.Range(startTimeBtwSpawnGold / -2, startTimeBtwSpawnGold / 2);
-
-
-                //     timeBtwSpawnGolden = startTimeBtwSpawnGold;
-                // }
-                // else
-                // {
-                //     timeBtwSpawnGolden -= Time.deltaTime;
-                // }
-
-                // // SPAWN NORMAL PIG
-                // if (timeBtwSpawn <= 0 + rndSpawnTime)
-                // {
-                //     spawnPos = new Vector2(transform.position.x, Random.Range(-maxHeight, maxHeight));
-
-                //     Instantiate(normalPig, spawnPos, Quaternion.identity);
-                //     rndSpawnTime = Random.Range(startTimeBtwSpawn / -2, startTimeBtwSpawn / 2);
-
-                //     timeBtwSpawn = startTimeBtwSpawn;
-                // }
-                // else
-                // {
-                //     timeBtwSpawn -= Time.deltaTime;
-                // }
-
-                // // SPAWN WILD PIG 
-                // if (timeBtwSpawnWild <= 0 + rndSpawnTime)
-                // {
-                //     spawnPos = new Vector2(transform.position.x, Random.Range(-maxHeight, maxHeight));
-
-                //     Instantiate(wildPig, spawnPos, Quaternion.identity);
-                //     rndSpawnTimeWild = Random.Range(startTimeBtwSpawnWild / -2, startTimeBtwSpawnWild / 2);
-
-                //     timeBtwSpawnWild = startTimeBtwSpawnWild;
-                // }
-                // else
-                // {
-                //     timeBtwSpawnWild -= Time.deltaTime;
-                // }
         }
 
     }
